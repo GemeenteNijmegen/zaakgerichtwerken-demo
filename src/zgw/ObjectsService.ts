e23486e2584e6bfedc07eec20bab77be818bd584
Marnix Dessing <m.dessing@nijmegen.nl>
2024-07-25 17:15:05 +0200

    fix: celery container does not need to be exposed

--- src/zgw/ObjectsService.ts
+++ src/zgw/ObjectsService.ts
@@ -11,6 +11,7 @@ export interface ObjectsServiceProps extends ComposedZgwServiceProps {}
 
 export class ObjectsService extends ComposedZgwService {
 
+  private static readonly PORT = 8080;
 
   constructor(scope: Construct, id: string, props: ObjectsServiceProps) {
     super(scope, id, props);
@@ -26,7 +27,7 @@ export class ObjectsService extends ComposedZgwService {
         zgwCluster: props.zgwCluster,
         expose: {
           path: 'objects',
-          port: 8080,
+          port: ObjectsService.PORT,
           priority: 20, // Note must be unique across all alb rules
         },
         initContainerCommand: ['/setup_configuration.sh'],
@@ -41,11 +42,6 @@ export class ObjectsService extends ComposedZgwService {
         environment: this.getEnvironmentConfiguration(),
         secrets: this.getSecretConfiguration(),
         zgwCluster: props.zgwCluster,
-        expose: {
-          path: 'objects',
-          port: 8080,
-          priority: 20, // Note must be unique across all alb rules
-        },
         command: ['/celery_worker.sh'],
       }),
     );
@@ -59,11 +55,11 @@ export class ObjectsService extends ComposedZgwService {
       DB_HOST: StringParameter.valueForStringParameter(this, Statics.ssmDbHostname),
       DB_PORT: StringParameter.valueForStringParameter(this, Statics.ssmDbPort),
       ALLOWED_HOSTS: '*', // See loadbalancer target remark above this.props.zgwCluster.alb.getDomain(),
-      CACHE_DEFAULT: this.props.zgwCluster.redis.redisCluster.attrRedisEndpointAddress + ':' + this.props.zgwCluster.redis.redisCluster.attrRedisEndpointPort + '/4',
-      CACHE_AXES: this.props.zgwCluster.redis.redisCluster.attrRedisEndpointAddress + ':' + this.props.zgwCluster.redis.redisCluster.attrRedisEndpointPort + '/4',
+      CACHE_DEFAULT: this.props.zgwCluster.redis.redisCluster.attrRedisEndpointAddress + ':' + this.props.zgwCluster.redis.redisCluster.attrRedisEndpointPort + '/objects-cache',
+      CACHE_AXES: this.props.zgwCluster.redis.redisCluster.attrRedisEndpointAddress + ':' + this.props.zgwCluster.redis.redisCluster.attrRedisEndpointPort + '/objects-cache',
       SUBPATH: '/objects',
       IS_HTTPS: 'True',
-      UWSGI_PORT: '8080',
+      UWSGI_PORT: ObjectsService.PORT.toString(),
 
       LOG_LEVEL: 'DEBUG',
       LOG_REQUESTS: 'True',
@@ -85,8 +81,8 @@ export class ObjectsService extends ComposedZgwService {
       OBJECTS_SUPERUSER_PASSWORD: 'admin',
 
       // Celery
-      CELERY_BROKER_URL: this.props.zgwCluster.redis.redisCluster.attrRedisEndpointAddress + ':' + this.props.zgwCluster.redis.redisCluster.attrRedisEndpointPort + '/5',
-      CELERY_RESULT_BACKEND: this.props.zgwCluster.redis.redisCluster.attrRedisEndpointAddress + ':' + this.props.zgwCluster.redis.redisCluster.attrRedisEndpointPort + '/5',
+      CELERY_BROKER_URL: this.props.zgwCluster.redis.redisCluster.attrRedisEndpointAddress + ':' + this.props.zgwCluster.redis.redisCluster.attrRedisEndpointPort + '/objects-celery',
+      CELERY_RESULT_BACKEND: this.props.zgwCluster.redis.redisCluster.attrRedisEndpointAddress + ':' + this.props.zgwCluster.redis.redisCluster.attrRedisEndpointPort + '/objects-celery',
       CELERY_LOGLEVEL: 'DEBUG',
 
     };
