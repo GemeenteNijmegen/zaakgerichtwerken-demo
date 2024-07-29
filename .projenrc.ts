b5095544ff2bc5e1f3ba5460d3685e3880649755
Marnix Dessing <m.dessing@nijmegen.nl>
2024-07-29 16:14:51 +0200

    feat: a lot of scripting to bootstrap our catalog

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,8 +2,8 @@ import { GemeenteNijmegenCdkApp } from '@gemeentenijmegen/projen-project-type';
 const project = new GemeenteNijmegenCdkApp({
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
-  devDeps: ['@gemeentenijmegen/projen-project-type', '@gemeentenijmegen/aws-constructs', '@types/pg'],
-  deps: ['@gemeentenijmegen/aws-constructs', 'pg', '@aws/pdk'],
+  devDeps: ['@gemeentenijmegen/projen-project-type', '@gemeentenijmegen/aws-constructs', '@types/pg', 'dotenv'],
+  deps: ['@gemeentenijmegen/aws-constructs', 'pg', '@aws/pdk', 'jsonwebtoken', '@types/jsonwebtoken'],
   name: 'zaakgerichtwerken',
   projenrcTs: true,
 });
