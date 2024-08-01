603d07fa55bd0d2d4f81d869e5048a7e74cdbfd0
Marnix Dessing <m.dessing@nijmegen.nl>
2024-08-01 11:00:05 +0200

    fix: joost made a mess

--- test/run.test.ts
+++ test/run.test.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
-<<<<<<< HEAD
 import { createEnkelVoudigInformatieObject } from '../scripts/CreateDocument';
+import { run } from '../scripts/CreateObjecttypes';
 import { addStatusToZaak, createZaak } from '../scripts/CreateZaak';
 import { generateCatalogus } from '../scripts/GenerateCatalogus';
 
@@ -16,19 +16,8 @@ test('2run', async () => {
 test('create document', async() => {
   await createEnkelVoudigInformatieObject('https://lb.zgw.sandbox-marnix.csp-nijmegen.nl/open-zaak/catalogi/api/v1/informatieobjecttypen/43d43290-8ead-410f-a2d4-8048842bdcf6');
 });
-=======
-import { run } from '../scripts/CreateObjecttypes';
 
-// xtest('run', async () => {
-//   await generateCatalogus();
-// });
-
-// xtest('run2', async () => {
-//   const zaak = await createZaak();
-//   await addStatusToZaak(zaak, 'https://lb.zgw.sandbox-marnix.csp-nijmegen.nl/open-zaak/catalogi/api/v1/statustypen/2e0edc16-fb56-4b3d-a227-3874d01003f5');
-// });
 
 test('Run', async () => {
   await run();
 });
->>>>>>> d921b626ff2c9dd9c329105389b91be3eca44994
