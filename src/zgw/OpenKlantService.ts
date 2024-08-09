63d15d5dcd3c3a1ae9c79c8ef5a6281b71a3af15
Marnix Dessing <m.dessing@nijmegen.nl>
2024-08-09 12:10:15 +0200

    chore: setup superuser in openklant

--- src/zgw/OpenKlantService.ts
+++ src/zgw/OpenKlantService.ts
@@ -31,7 +31,7 @@ export class OpenKlantService extends ComposedZgwService {
           priority: 24, // Note must be unique across all alb rules
         },
         // Note: DB should be created before this command can run
-        // initContainerCommand: ['python', 'src/manage.py', 'createsuperuser', '--no-input', '--username', 'mdessing', '--email', 'm.dessing@nijmegen.nl', '--skip-checks'],
+        initContainerCommand: ['python', 'src/manage.py', 'createsuperuser', '--no-input', '--username', 'mdessing', '--email', 'm.dessing@nijmegen.nl', '--skip-checks'],
       }),
     );
 
